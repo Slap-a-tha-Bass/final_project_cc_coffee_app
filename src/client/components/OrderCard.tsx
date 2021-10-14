d2aa6cadadaed72de639c831d58b8ccaf2cce9e7
Corey DeLoach <cdeloach16@gmail.com>
2021-10-14 16:08:25 -0500

    more css, possibly solved undefined error in vieworder / ordercard

--- src/client/components/OrderCard.tsx
+++ src/client/components/OrderCard.tsx
@@ -8,15 +8,16 @@ const OrderCard = ({ id, first_name, drink_id, snack_id, price, isPreview, in_pr
     const history = useHistory();
     const [drink_name, setDrink_name] = useState<Drinks['name']>('');
     const [snack_name, setSnack_name] = useState<Snacks['name']>('');
+
     useEffect(() => {
-        apiService(`/api/drinks/${drink_id}`)
+        apiService(`/api/drinks/${drink_id || ''}`)
             .then(data => setDrink_name(data.name));
     }, [drink_id]);
     useEffect(() => {
-        apiService(`/api/snacks/${snack_id}`)
+        apiService(`/api/snacks/${snack_id || ''}`)
             .then(data => setSnack_name(data.name));
     }, [snack_id]);
-    const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleViewOrder = (e: React.MouseEvent<HTMLButtonElement>) => {
         Swal.fire({
             title: `${first_name}'s order received!`,
             icon: 'success',
@@ -25,7 +26,15 @@ const OrderCard = ({ id, first_name, drink_id, snack_id, price, isPreview, in_pr
         });
         history.push(`/orders/${id}`);
     }
-    
+    const handleOrderComplete = (e: React.MouseEvent<HTMLButtonElement>) => {
+        Swal.fire({
+            title: `${first_name}'s order completed!`,
+            icon: 'success',
+            timer: 2000,
+            showConfirmButton: false
+        });
+        history.push(`/orders/${id}`);
+    }
     return (
         <div className="card bg-info p-2 border rounded shadow my-2">
             <h1 className="card-title text-center text-light border-3 border-bottom border-light mb-2"><i className="bi bi-braces"></i>  {first_name}</h1>
@@ -34,12 +43,13 @@ const OrderCard = ({ id, first_name, drink_id, snack_id, price, isPreview, in_pr
                 <h3 className="card-text text-center text-light"><i className="bi bi-palette-fill"></i>  {snack_name}</h3>
                 <h5 className="card-text text-center text-light">${price}</h5>
             </div>
-            <div className="d-flex justify-content-between">
-                {isPreview && <Link className="btn btn-primary text-info font-weight-bolder border border-light" to={`/edit/${id}`}>edit order</Link>}
-                {isPreview && <button className="btn btn-primary text-info font-weight-bolder border border-light">delete order</button>}
+            <div className="d-flex justify-content-around">
+                {isPreview && <Link className="btn btn-primary btn-sm text-info border border-light d-flex align-items-center" to={`/edit/${id}`}>edit</Link>}
+                {isPreview && <button className="btn btn-primary btn-sm text-info border border-light d-flex align-items-center">delete</button>}
+                {is_finished && <button className="btn btn-info btn-lg" onClick={handleOrderComplete}><i className="bi bi-arrow-right-circle-fill"></i></button>}
             </div>
             <div className="d-flex justify-content-end">
-                {in_progress && <div className="form-check form-switch mx-4"><input onChange={handleToggle} className="form-check-input" type="checkbox" /></div>}
+                {in_progress && <button className="btn btn-info btn-lg" onClick={handleViewOrder}><i className="bi bi-arrow-right-circle-fill"></i></button>}
             </div>
         </div>
     )
--- src/client/views/ViewOrder.tsx
+++ src/client/views/ViewOrder.tsx
@@ -7,18 +7,18 @@ import { apiService } from '../utils/api-service';
 const ViewOrder = () => {
     const { id } = useParams<{ id: string }>();
     const [orders, setOrders] = useState<Orders>();
-    const [drink_id, setDrink_id] = useState<Orders['drink_id']>();
-    const [snack_id, setSnack_id] = useState<Orders['snack_id']>();
+
     useEffect(() => {
         apiService(`/api/orders/${id}`)
             .then(data => {
+                console.log(data)
                 setOrders(data)});
     }, [id]);
     
 
     return (
         <div>
-            <OrderCard {...orders} key={id} isPreview />
+            <OrderCard {...orders} key={id} isPreview is_finished />
         </div>
     )
 }
